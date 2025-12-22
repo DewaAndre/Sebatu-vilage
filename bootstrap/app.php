bad993783b750368e01b847b72826cf743637d59
Dewa Gede Andrean Agus Arindika <142086350+DewaAndre@users.noreply.github.com>
2025-12-22 14:02:29 +0800

    fix: disable maintenance middleware for vercel

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,6 +3,7 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Foundation\Http\Middleware\PreventRequestsDuringMaintenance;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -11,8 +12,10 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        //
+        // 🔥 PENTING: matikan maintenance middleware (WAJIB di Vercel)
+        $middleware->remove(PreventRequestsDuringMaintenance::class);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
-    })->create();
+    })
+    ->create();
