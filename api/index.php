11ab3422ae457a2165e8516ac78ee5ec21ac666a
Dewa Gede Andrean Agus Arindika <142086350+DewaAndre@users.noreply.github.com>
2025-12-22 13:58:43 +0800

    FINISH

--- api/index.php
+++ api/index.php
@@ -1,11 +1,3 @@
 <?php
 
-$_ENV['APP_ENV'] = 'production';
-$_ENV['APP_DEBUG'] = 'false';
-
-$_ENV['VIEW_COMPILED_PATH'] = '/tmp/views';
-$_ENV['APP_CONFIG_CACHE'] = '/tmp/config.php';
-$_ENV['APP_SERVICES_CACHE'] = '/tmp/services.php';
-$_ENV['APP_PACKAGES_CACHE'] = '/tmp/packages.php';
-
-require __DIR__ . '/../public/index.php';
+require __DIR__.'/../public/index.php';
