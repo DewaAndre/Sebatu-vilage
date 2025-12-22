22a8466110285bcb26a25045232da3b457a3cb51
Dewa Gede Andrean Agus Arindika <142086350+DewaAndre@users.noreply.github.com>
2025-12-22 14:16:58 +0800

    fix: laravel response send for vercel

--- public/index.php
+++ public/index.php
@@ -5,16 +5,23 @@ use Illuminate\Http\Request;
 
 define('LARAVEL_START', microtime(true));
 
-// Determine if the application is in maintenance mode...
+// Maintenance mode
 if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
     require $maintenance;
 }
 
-// Register the Composer autoloader...
+// Autoload
 require __DIR__.'/../vendor/autoload.php';
 
-// Bootstrap Laravel and handle the request...
+// Bootstrap app
 /** @var Application $app */
 $app = require_once __DIR__.'/../bootstrap/app.php';
 
-$app->handleRequest(Request::capture());
+// HANDLE REQUEST MANUAL (WAJIB UNTUK VERCEL)
+$request = Request::capture();
+$response = $app->handle($request);
+
+// ⛔ INI YANG KEMARIN HILANG
+$response->send();
+
+$app->terminate($request, $response);
