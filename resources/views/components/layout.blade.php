33959bdca1227559d889670915e3e7e8da05e088
Dewa Gede Andrean Agus Arindika <142086350+DewaAndre@users.noreply.github.com>
2025-12-22 16:22:28 +0800

    fix: load vite css and js for tailwind on vercel

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -4,22 +4,24 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Desa Wisata Sebatu</title>
+
     <link rel="icon" type="image/png" href="{{ asset('img/logo2.png') }}">
     <link rel="stylesheet" href="https://rsms.me/inter/inter.css">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" rel="stylesheet">
-    <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
-    @vite('resources/css/app.css')
+
+    @vite([
+        'resources/css/app.css',
+        'resources/js/app.js'
+    ])
 </head>
+
 <body class="font-sans antialiased bg-gray-100">
     <div class="flex min-h-screen">
-        <!-- Sidebar -->
         @include('components.navbar')
 
-        <!-- Main Content -->
         <main class="flex-1">
             {{ $slot }}
         </main>
     </div>
-    
 </body>
 </html>
