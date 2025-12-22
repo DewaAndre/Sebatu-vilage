67693f733f463a002f0c563244dfce74c9dc75c8
Dewa Gede Andrean Agus Arindika <142086350+DewaAndre@users.noreply.github.com>
2025-12-22 13:50:02 +0800

    fix: move laravel cache to /tmp for vercel

--- api/index.php
+++ api/index.php
@@ -1,18 +1,11 @@
 <?php
 
-// ===== FORCE ENV (Vercel does NOT load .env) =====
 $_ENV['APP_ENV'] = 'production';
-$_ENV['APP_DEBUG'] = false;
-$_SERVER['APP_DEBUG'] = false;
+$_ENV['APP_DEBUG'] = 'false';
 
-// WAJIB: APP_KEY harus ada
-$_ENV['APP_KEY'] = 'base64:yYSWcePz5gR9Bi7eLgK+cg6Egof8YyKyIHZ03N9nwYo=';
+$_ENV['VIEW_COMPILED_PATH'] = '/tmp/views';
+$_ENV['APP_CONFIG_CACHE'] = '/tmp/config.php';
+$_ENV['APP_SERVICES_CACHE'] = '/tmp/services.php';
+$_ENV['APP_PACKAGES_CACHE'] = '/tmp/packages.php';
 
-// Disable error view system
-$_ENV['VIEW_COMPILED_PATH'] = '/tmp';
-
-// Prevent Laravel from rendering error views
-$_SERVER['LARAVEL_START'] = microtime(true);
-
-// Boot Laravel
 require __DIR__ . '/../public/index.php';
